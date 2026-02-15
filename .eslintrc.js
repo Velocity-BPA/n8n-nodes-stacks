4e89487324a38fd293a19ce1391aa73a94d0f247
John Drefahl <john@velobpa.com>
2026-02-15 03:27:28 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,15 @@
-/**
- * Copyright (c) Velocity BPA, LLC
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
   root: true,
-  env: {
-    browser: false,
-    es6: true,
-    node: true,
-  },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-    sourceType: 'module',
-    extraFileExtensions: ['.json'],
-  },
-  ignorePatterns: [
-    '.eslintrc.js',
-    '**/*.js',
-    '**/node_modules/**',
-    '**/dist/**',
-    'gulpfile.js',
-  ],
-  plugins: ['@typescript-eslint', 'n8n-nodes-base'],
-  extends: ['plugin:n8n-nodes-base/nodes'],
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
   rules: {
-    'n8n-nodes-base/node-dirname-against-convention': 'off',
-    'n8n-nodes-base/node-class-description-inputs-wrong-regular-node': 'off',
-    'n8n-nodes-base/node-class-description-outputs-wrong': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
   },
 };
